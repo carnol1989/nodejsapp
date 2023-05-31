9ed1e6ce9b20d581ee6eed1a0a156b6e272bafa6
Carlos Nole <carnole1989@outlook.com>
2023-05-30 22:46:52 -0500

    chore: update info repo

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/carnol1989/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('carnole1989')
+            node / gitConfigEmail('carnole1989@outlook.com')
         }
     }
     triggers {
